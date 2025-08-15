fbf715f40551d68a2940b57d1cbd4f3b321ed1f3
Lin Wang <wanglinseven@gmail.com>
2025-08-15 11:34:27 -0400

    fix some warnings

--- src/main.rs
+++ src/main.rs
@@ -38,34 +38,34 @@ macro_rules! offset_of {
     }};
 }
 
-fn array_size<T>(a: &[T]) -> usize {
-    a.len() * std::mem::size_of::<T>()
-}
-
-unsafe extern "system" fn vulkan_debug_callback(
+extern "system" fn vulkan_debug_callback(
     message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
     message_type: vk::DebugUtilsMessageTypeFlagsEXT,
     p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT<'_>,
     _user_data: *mut std::os::raw::c_void,
 ) -> vk::Bool32 {
-    let callback_data = *p_callback_data;
+    let callback_data = unsafe { *p_callback_data };
     let message_id_number = callback_data.message_id_number;
 
-    let message_id_name = if callback_data.p_message_id_name.is_null() {
-        Cow::from("?")
-    } else {
-        ffi::CStr::from_ptr(callback_data.p_message_id_name).to_string_lossy()
-    };
+    if message_severity == vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
+        || message_severity == vk::DebugUtilsMessageSeverityFlagsEXT::ERROR
+    {
+        let message_id_name = if callback_data.p_message_id_name.is_null() {
+            Cow::from("?")
+        } else {
+            unsafe { ffi::CStr::from_ptr(callback_data.p_message_id_name).to_string_lossy() }
+        };
 
-    let message = if callback_data.p_message.is_null() {
-        Cow::from("?")
-    } else {
-        ffi::CStr::from_ptr(callback_data.p_message).to_string_lossy()
-    };
+        let message = if callback_data.p_message.is_null() {
+            Cow::from("?")
+        } else {
+            unsafe { ffi::CStr::from_ptr(callback_data.p_message).to_string_lossy() }
+        };
 
-    println!(
-        "[{message_severity:?}:{message_type:?}] [{message_id_name} ({message_id_number})] : {message}\n",
-    );
+        println!(
+            "[{message_severity:?}:{message_type:?}] [{message_id_name} ({message_id_number})] : {message}\n",
+        );
+    }
 
     vk::FALSE
 }
