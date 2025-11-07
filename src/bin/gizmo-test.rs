3e12a691642512ab8a4369a07485748ead52236e
Lin Wang <wanglinseven@gmail.com>
2025-11-07 16:30:59 -0500

    add a triangle to gizmo-test

--- src/bin/gizmo-test.rs
+++ src/bin/gizmo-test.rs
@@ -43,6 +43,15 @@ struct GizmoArrowPass {
 }
 
 #[derive(Default)]
+struct TrianglePass {
+    program: Program,
+    mesh: Mesh,
+    pipeline: vk::Pipeline,
+    desc_sets: Vec<vk::DescriptorSet>,
+    uniform_buf: Buffer,
+}
+
+#[derive(Default)]
 struct GizmoCirclePass {
     program: Program,
     // arch: Mesh,
@@ -59,7 +68,7 @@ struct GizmoCirclePass {
 #[derive(Default)]
 struct GizmoTest {
     renderer: Option<Renderer>,
-    triangle: Mesh,
+    triangle_pass: TrianglePass,
     gizmo_arrow: GizmoArrowPass,
     gizmo_arch: GizmoCirclePass,
 }
@@ -266,7 +275,7 @@ impl GizmoArrowPass {
         let color_attachment_infos = [vk::RenderingAttachmentInfo::default()
             .image_view(renderer.present_image_view())
             .image_layout(vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL)
-            .load_op(vk::AttachmentLoadOp::CLEAR)
+            .load_op(vk::AttachmentLoadOp::DONT_CARE)
             .store_op(vk::AttachmentStoreOp::STORE)
             .clear_value(vk::ClearValue {
                 color: vk::ClearColorValue {
@@ -745,11 +754,243 @@ impl GizmoCirclePass {
     }
 }
 
+impl TrianglePass {
+    const PER_FRAME_UNIFORM_DATA_SIZE: usize = 64;
+
+    fn new(renderer: &Renderer) -> Self {
+        let program = Program::new(
+            &renderer.vkbase.device,
+            vk::PipelineBindPoint::GRAPHICS,
+            vec![
+                Rc::clone(renderer.shader_set.get("triangle.vert").unwrap()),
+                Rc::clone(renderer.shader_set.get("triangle.frag").unwrap()),
+            ],
+        );
+        let mesh = Mesh::from_obj(
+            &renderer.vkbase.device,
+            &renderer.vkbase.device_memory_properties,
+            "assets/meshes/triangle.obj",
+        );
+
+        let pipeline = {
+            let vertex_binding_descs = [vk::VertexInputBindingDescription::default()
+                .binding(0)
+                .stride(size_of::<Vertex>() as u32)
+                .input_rate(vk::VertexInputRate::VERTEX)];
+
+            let vertex_attrib_descs = [
+                vk::VertexInputAttributeDescription {
+                    location: 0,
+                    binding: 0,
+                    format: vk::Format::R32G32B32_SFLOAT,
+                    offset: offset_of!(Vertex, pos) as u32,
+                },
+                vk::VertexInputAttributeDescription {
+                    location: 1,
+                    binding: 0,
+                    format: vk::Format::R32G32B32A32_SFLOAT,
+                    offset: offset_of!(Vertex, color) as u32,
+                },
+            ];
+
+            let vertex_input_state = vk::PipelineVertexInputStateCreateInfo::default()
+                .vertex_binding_descriptions(&vertex_binding_descs)
+                .vertex_attribute_descriptions(&vertex_attrib_descs);
+
+            let color_attachment_state = [vk::PipelineColorBlendAttachmentState::default()
+                .blend_enable(false)
+                .color_write_mask(vk::ColorComponentFlags::RGBA)];
+
+            let color_blend_state = vk::PipelineColorBlendStateCreateInfo::default()
+                .attachments(&color_attachment_state);
+
+            PipelineBuilder::new(
+                &renderer.vkbase.device,
+                &program,
+                &vertex_input_state,
+                &[renderer.vkbase.surface_format.format],
+                &color_blend_state,
+            )
+            .depth_format(renderer.vkbase.depth_format)
+            .build()
+        };
+
+        let uniform_buf = {
+            Buffer::new(
+                &renderer.vkbase.device,
+                (Self::PER_FRAME_UNIFORM_DATA_SIZE * MAX_FRAMES_IN_FLIGHT) as u64,
+                vk::BufferUsageFlags::UNIFORM_BUFFER,
+                &renderer.vkbase.device_memory_properties,
+            )
+        };
+
+        let desc_sets = {
+            let desc_set_alloc_info = vk::DescriptorSetAllocateInfo::default()
+                .set_layouts(program.desc_set_layouts())
+                .descriptor_pool(renderer.desc_pool);
+
+            unsafe {
+                let desc_sets = renderer
+                    .vkbase
+                    .device
+                    .allocate_descriptor_sets(&desc_set_alloc_info)
+                    .unwrap();
+
+                let desc_buf_infos = [vk::DescriptorBufferInfo::default()
+                    .buffer(uniform_buf.buf)
+                    .offset(0)
+                    .range(Self::PER_FRAME_UNIFORM_DATA_SIZE as u64)];
+                let desc_writes = [vk::WriteDescriptorSet::default()
+                    .descriptor_type(vk::DescriptorType::UNIFORM_BUFFER_DYNAMIC)
+                    .dst_set(desc_sets[0])
+                    .dst_binding(0)
+                    .dst_array_element(0)
+                    .buffer_info(&desc_buf_infos)];
+                renderer
+                    .vkbase
+                    .device
+                    .update_descriptor_sets(&desc_writes, &[]);
+
+                desc_sets
+            }
+        };
+
+        Self {
+            program,
+            mesh,
+            pipeline,
+            desc_sets,
+            uniform_buf,
+        }
+    }
+
+    fn update(&mut self, in_flight_frame_index: usize, pers_view_matrix: &Mat4) {
+        let per_frame_uniform_data_offset =
+            in_flight_frame_index * Self::PER_FRAME_UNIFORM_DATA_SIZE;
+        self.uniform_buf
+            .copy_value(per_frame_uniform_data_offset, pers_view_matrix);
+    }
+
+    fn draw(&self, renderer: &Renderer) {
+        let color_attachment_infos = [vk::RenderingAttachmentInfo::default()
+            .image_view(renderer.present_image_view())
+            .image_layout(vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL)
+            .load_op(vk::AttachmentLoadOp::CLEAR)
+            .store_op(vk::AttachmentStoreOp::STORE)
+            .clear_value(vk::ClearValue {
+                color: vk::ClearColorValue {
+                    float32: [135.0 / 255.0, 206.0 / 255.0, 250.0 / 255.0, 15.0 / 255.0],
+                },
+            })];
+        let depth_attachment_info = vk::RenderingAttachmentInfo::default()
+            .image_view(renderer.depth_image_view())
+            .image_layout(vk::ImageLayout::DEPTH_ATTACHMENT_OPTIMAL)
+            .load_op(vk::AttachmentLoadOp::CLEAR)
+            .store_op(vk::AttachmentStoreOp::STORE)
+            .clear_value(vk::ClearValue {
+                depth_stencil: vk::ClearDepthStencilValue {
+                    depth: 0.0,
+                    stencil: 0,
+                },
+            });
+
+        let image_extent = renderer.vkbase.swapchain.image_extent();
+
+        let rendering_info = vk::RenderingInfo::default()
+            .render_area(vk::Rect2D {
+                offset: vk::Offset2D { x: 0, y: 0 },
+                extent: image_extent,
+            })
+            .layer_count(1)
+            .color_attachments(&color_attachment_infos)
+            .depth_attachment(&depth_attachment_info);
+
+        let viewport = vk::Viewport {
+            x: 0.0,
+            y: 0.0,
+            width: image_extent.width as f32,
+            height: image_extent.height as f32,
+            min_depth: 0.0,
+            max_depth: 1.0,
+        };
+        let scissor: vk::Rect2D = image_extent.into();
+
+        unsafe {
+            let cmd_buf = renderer.curr_cmd_cuf();
+
+            renderer
+                .vkbase
+                .device
+                .cmd_begin_rendering(cmd_buf, &rendering_info);
+
+            renderer.vkbase.device.cmd_bind_pipeline(
+                cmd_buf,
+                vk::PipelineBindPoint::GRAPHICS,
+                self.pipeline,
+            );
+
+            renderer
+                .vkbase
+                .device
+                .cmd_set_viewport(cmd_buf, 0, &[viewport]);
+            renderer
+                .vkbase
+                .device
+                .cmd_set_scissor(cmd_buf, 0, &[scissor]);
+
+            renderer.vkbase.device.cmd_bind_vertex_buffers(
+                cmd_buf,
+                0,
+                &[self.mesh.vertex_buffer.buf],
+                &[0],
+            );
+            renderer.vkbase.device.cmd_bind_index_buffer(
+                cmd_buf,
+                self.mesh.index_buffer.buf,
+                0,
+                vk::IndexType::UINT32,
+            );
+
+            renderer.vkbase.device.cmd_bind_descriptor_sets(
+                cmd_buf,
+                self.program.bind_point,
+                self.program.pipeline_layout,
+                0,
+                &self.desc_sets,
+                &[(renderer.in_flight_frame_index() * Self::PER_FRAME_UNIFORM_DATA_SIZE) as u32],
+            );
+
+            for submesh in &self.mesh.submeshes {
+                renderer.vkbase.device.cmd_draw_indexed(
+                    cmd_buf,
+                    submesh.index_count,
+                    1,
+                    submesh.index_offset,
+                    submesh.vertex_offset,
+                    0,
+                );
+            }
+
+            renderer.vkbase.device.cmd_end_rendering(cmd_buf);
+        }
+    }
+
+    fn destruct(&mut self, device: &Device) {
+        self.uniform_buf.destruct(device);
+        unsafe {
+            device.destroy_pipeline(self.pipeline, None);
+        }
+        self.mesh.destruct(device);
+        self.program.destruct(device);
+    }
+}
+
 impl Scene for GizmoTest {
     fn init(&mut self, window: &Window) {
         let renderer = Renderer::new(window);
         self.gizmo_arrow = GizmoArrowPass::new(&renderer);
         self.gizmo_arch = GizmoCirclePass::new(&renderer);
+        self.triangle_pass = TrianglePass::new(&renderer);
         self.renderer = Some(renderer);
     }
 
@@ -763,15 +1004,17 @@ impl Scene for GizmoTest {
         let image_extent = renderer.vkbase.swapchain.image_extent();
         let image_aspect_ratio = (image_extent.width as f32) / (image_extent.height as f32);
         let pv_matrix = camera.ny_pers_view_matrix(image_aspect_ratio);
-        let pvm = pv_matrix * scale_matrix;
+        let pvsm = pv_matrix * scale_matrix;
 
+        self.triangle_pass.update(renderer.in_flight_frame_index(), &pv_matrix);
         self.gizmo_arrow
-            .update(renderer.in_flight_frame_index(), &pvm);
+            .update(renderer.in_flight_frame_index(), &pvsm);
         self.gizmo_arch
-            .update(renderer.in_flight_frame_index(), &pvm, camera.position);
+            .update(renderer.in_flight_frame_index(), &pvsm, camera.position);
 
         renderer.begin_frame();
 
+        self.triangle_pass.draw(&renderer);
         self.gizmo_arrow.draw(renderer);
         self.gizmo_arch.draw(renderer);
 
@@ -795,6 +1038,7 @@ impl Scene for GizmoTest {
 
         {
             let device = &self.renderer.as_ref().unwrap().vkbase.device;
+            self.triangle_pass.destruct(device);
             self.gizmo_arch.destruct(device);
             self.gizmo_arrow.destruct(device);
         }
